9e71c42bcae5a6e3604a20eb66b24ac6f7eaadbd
chrisBow <gavinchristopher2017@gmail.com>
2018-09-11 20:57:17 +0100

    First commit: test plot and first theme.

---
+++ minimal_serif_navy.R
@@ -0,0 +1,20 @@
+theme_minimal_serif_navy <- function (base_size = 12, base_family = "serif", ticks = TRUE) 
+{
+  ret <- theme_bw(base_family = base_family, base_size = base_size) + 
+    theme(legend.background = element_blank(), legend.key = element_blank(), 
+          panel.border = element_blank(), 
+          strip.background = element_blank(), 
+          panel.background = element_rect(fill = "#ffffff", colour = NA),
+          plot.background = element_rect(fill = "#FFFFFF"),
+          axis.line = element_blank(), panel.grid = element_blank(),
+          axis.text.x = element_text(colour="#222C3F"),
+          axis.title.x = element_text(colour = "#222C3F"),
+          axis.title.y = element_text(colour="#222C3F"),
+          axis.text.y = element_text(colour="#222C3F"),
+          axis.title = element_text(colour = "#222C3F"),
+          plot.title = element_text(colour = "#222C3F"))
+  if (!ticks) {
+    ret <- ret + theme(axis.ticks = element_blank())
+  }
+  ret
+}
---
+++ test_plot.R
@@ -0,0 +1,11 @@
+library(ggplot2)
+
+ggplot(iris, aes(Sepal.Length, Sepal.Width,
+                 colour = Species)) +
+  geom_point() +
+  labs(title = "Sepal Width Against Sepal Length",
+       subtitle = "By species",
+       x = "Sepal length",
+       y = "Sepal width",
+       caption = "Data from 'Iris' dataset") +
+  theme_minimal_serif_navy()
